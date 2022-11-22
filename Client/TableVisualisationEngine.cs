2bcefa9630a6422ea479f95e9bd3741806edcc2c
Clyde <noneclyde@gmail.com>
2022-11-22 13:23:13 +0200

    Add TableVisualisationEngine class

---
+++ Client/TableVisualisationEngine.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleTableExt;
+
+namespace Client
+{
+    internal class TableVisualisationEngine
+    {
+        private readonly List<List<object>> TableData = new();
+
+        internal void Print()
+        {
+            ConsoleTableBuilder
+                .From(TableData)
+                .WithTitle("ShiftLogger-App", ConsoleColor.Yellow, ConsoleColor.Black)
+                .WithColumn("ID", "Start", "End", "Pay", "Minutes", "Location")
+                .ExportAndWriteLine();
+        }
+
+        internal void Add(List<API.Models.Shift> List)
+        {
+            foreach (API.Models.Shift Entity in List)
+            {
+                TableData.Add(
+                    new List<object>
+                    {
+                        Entity.ShiftID,
+                        Entity.Start,
+                        Entity.End,
+                        Entity.Pay,
+                        Entity.Minutes,
+                        Entity.Location
+                    }
+                );
+            }
+        }
+
+        internal void Clear()
+        {
+            TableData.Clear();
+        }
+    }
+}
