445fe1a669aab6156a8dd178c8b5afa09d03996e
Clyde <noneclyde@gmail.com>
2022-11-24 18:12:23 +0200

    Remove any validation from API

--- API/Models/Shift.cs
+++ API/Models/Shift.cs
@@ -3,24 +3,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace API.Models
 {
-    public class Shift : IValidatableObject
+    public class Shift
     {
         public int ShiftID { get; set; }
-        [Required]
         public DateTime Start { get; set; }
-        [Required]
         public DateTime End { get; set; }
-        [Required]
         public decimal Pay { get; set; }
-        [Required]
         public decimal Minutes { get; set; }
-        [Required]
         public string? Location { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Start.CompareTo(End) != -1)
-                yield return new ValidationResult("End date has to be greater than Start date");
-    }
     }
 }
